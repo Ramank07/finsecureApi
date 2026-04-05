cc80e3c164d5107a3630941d22f6b2564f61376b
Ramank07 <kumarraman9934057@example.com>
2026-04-05 22:48:28 +0530

    decrese token validity

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -120,7 +120,7 @@ export const login = async (req, res) => {
     const token = jwt.sign(
       { id: user._id, role: user.role, email: user.email },
       process.env.JWT_SECRET,
-      { expiresIn: "7d" }
+      { expiresIn: "30min" }
     );
 
     res.cookie("token", token, {
